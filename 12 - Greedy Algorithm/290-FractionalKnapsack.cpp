856d5d77616399f5b133886b71c6f5c42dde79e0
Rehan <rehnx95@gmail.com>
2026-07-18 21:02:26 +0530

    Solve 2 Greedy Problem

--- 12 - Greedy Algorithm/290-FractionalKnapsack.cpp
+++ 12 - Greedy Algorithm/290-FractionalKnapsack.cpp
@@ -3,5 +3,40 @@ using namespace std;
 
 int main()
 {
+    vector<int> v = {100, 60, 100, 200};
+    vector<int> w = {20, 10, 50, 50};
+    double cap = 90;
     
+    // Stores: {ratio, original_index}
+    vector<pair<double, int>> p;
+    double ans = 0;
+    
+    for (int i = 0; i < v.size(); i++)
+    {
+        p.push_back({(1.0 * v[i]) / w[i], i});
+    }
+    
+    // Sorts in descending order automatically
+    sort(p.begin(), p.end(), greater<pair<double, int>>());
+    
+    for (int i = 0; i < p.size(); i++)
+    {
+        if (cap == 0)
+            break;
+            
+        int idx = p[i].second; 
+        
+        if (w[idx] <= cap)
+        {
+            cap -= w[idx];
+            ans += v[idx];
+        }
+        else
+        {
+            double ratio = p[i].first;
+            ans += cap * ratio;
+            cap = 0;
+        }
+    }
+    cout << ans << endl;
 }
--- 12 - Greedy Algorithm/293-ValidParen.cpp
+++ 12 - Greedy Algorithm/293-ValidParen.cpp
@@ -1,7 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+bool recu(string s, int idx, int open)
+{
+    if (open < 0)
+        return false;
+    if (idx == s.size())
+        return open == 0;
+    if (s[idx] == '(')
+    {
+        return recu(s, idx + 1, open + 1);
+    }
+    else if (s[idx] == ')')
+    {
+        return recu(s, idx + 1, open - 1);
+    }
+    else
+    {
+        return recu(s, idx + 1, open + 1) ||
+               recu(s, idx + 1, open - 1) ||
+               recu(s, idx + 1, open);
+    }
+}
+bool brute(string s)
+{
+    return recu(s, 0, 0);
+}
+bool optimal(string s)
+{
+    int mn = 0, mx = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            mn++;
+            mx++;
+        }
+        else if (s[i] == ')')
+        {
+            mn--;
+            mx--;
+        }
+        else
+        {
+            mn--;
+            mx++;
+        }
+        if (mn < 0)
+            mn = 0;
+        if (mx < 0)
+            return false;
+    }
+    return mn == 0;
+}
 int main()
 {
-    
+    string s = "(*())(";
+    cout << brute(s) << endl;
+    cout << optimal(s) << endl;
 }
